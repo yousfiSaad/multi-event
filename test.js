113d4703390aca5a4297ea4f35bc2ea660711d88
SaadY <yousfi.saad@gmail.com>
2015-08-24 22:50:45 +0100

    1st test

--- test.js
+++ test.js
@@ -1,38 +1,14 @@
-import Eventy from './src/'
+var assert = require('assert');
+import EventEmitter from './src';
 
-let events = new Eventy();
-
-let createLogiFunction = function(id){
-  id--;
-  let digits = ['one', 'two', 'three', 'gang of foor'];
-  if(id < digits.length)
-    return function(from){
-      console.log(digits[id] + ' from ' + from);
-    }
-}
-
-let ftwo = createLogiFunction(2);
-
-events.on('one', createLogiFunction(1), 'one')
-
-.on('two', createLogiFunction(1), 'two')
-.on('two', createLogiFunction(2), 'two')
-
-.on('three', createLogiFunction(1), 'three')
-.on('three', ftwo, 'three')
-.on('three', createLogiFunction(3), 'three')
-
-
-.emit('one')
-
-.emit('two')
-
-.emit('three')
-
-.off('three', ftwo)
-
-.off('two')
-
-.emit('two')
-
-.emit('three')
+describe('Eventy', () => {
+  it('#on() and #emit()', (done) => {
+    let eventEmitter = new EventEmitter();
+    var originalEvent = {};
+    eventEmitter.on('boom', function(event) {
+      assert(event, originalEvent);
+      done();
+    });
+    eventEmitter.emit('boom', originalEvent);
+  });
+});
